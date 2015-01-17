5282c65b032a8fae838041065e843ebceaeb8974
namtax <namtax@hotmail.com>
2015-01-17 18:22:53 +0000

    add class to parse input

---
+++ lib/file_parser.rb
@@ -0,0 +1,66 @@
+class FileParser
+  attr_reader :coords, :input
+
+  REQUIRED_NUMBER_OF_COMMANDS = 6
+  REQUIRED_NUMBER_OF_COORDS   = 5
+  INSTRUCTION_WHITELIST = /(N|S|E|W)/
+
+  def initialize
+    raise error_message unless File.exists?(file_path)
+    @input  = File.readlines(file_path).map(&:strip)
+    @coords = fetch_coords
+    validate
+  end
+
+  def room_dimensions
+    @coords[0]
+  end
+
+  def hoover_pos
+    @coords[1]
+  end
+
+  def dirt_patches
+    @coords[2..-1]
+  end
+
+  def instructions
+    input.last
+  end
+
+  private
+
+  def error_message
+    'Please store input file at /bin/input.txt'
+  end
+
+  def file_path
+    'bin/input.txt'
+  end
+
+  def fetch_coords
+    input[0...-1].map{ |i| i.split.map(&:to_i) }
+  end
+
+  def validate
+    raise 'Please provide full input - see readme!' unless valid_input?
+    raise 'Please provide valid coords - see readme!' unless valid_coords?
+    raise 'Please provide valid instructions - see readme!' unless valid_instructions?
+  end
+
+  def valid_input?
+    input.size == REQUIRED_NUMBER_OF_COMMANDS
+  end
+
+  def valid_coords?
+    (input_coords.grep(/^(\d) (\d)$/).count == REQUIRED_NUMBER_OF_COORDS)
+  end
+
+  def input_coords
+    input[0...-1]
+  end
+
+  def valid_instructions?
+    instructions.each_char.all?{ |x| x =~ INSTRUCTION_WHITELIST }
+  end
+end
---
+++ spec/lib/file_parser_spec.rb
@@ -0,0 +1,36 @@
+describe FileParser do 
+  subject { FileParser.new }
+
+  it { expect(subject.dirt_patches).to eq [[1, 0], [2, 2], [2, 3]] }
+  it { expect(subject.room_dimensions).to eq [5, 5] }
+  it { expect(subject.hoover_pos).to eq [1, 2] }
+  it { expect(subject.instructions).to eq %q[NNESEESWNWW] }
+
+  context 'file missing' do 
+    before { allow_any_instance_of(FileParser).to receive(:file_path).and_return('bin/doesnt_exist') }
+    it     { expect{subject}.to raise_error('Please store input file at /bin/input.txt') }
+  end
+
+  context 'input is missing commands' do 
+    before { allow(File).to receive(:readlines).and_return(["5 5\n"]) }
+    it     { expect{subject}.to raise_error('Please provide full input - see readme!') }
+  end
+
+  context 'input contains invalid co-ordinates' do 
+    let(:invalid_coords) { ["5 X\n", "1 2\n", "1 0\n", "2 2\n", "2 3\n", "NNESEESWNWW"] } 
+    before { allow(File).to receive(:readlines).and_return(invalid_coords) }
+    it     { expect{subject}.to raise_error('Please provide valid coords - see readme!') }
+  end
+
+  context 'input contains missing co-ordinates' do 
+    let(:invalid_coords) { ["5", "1 2\n", "1 0\n", "2 2\n", "2 3\n", "NNESEESWNWW"] } 
+    before { allow(File).to receive(:readlines).and_return(invalid_coords) }
+    it     { expect{subject}.to raise_error('Please provide valid coords - see readme!') }
+  end
+
+  context 'input contains invalid instructions' do 
+    let(:invalid_coords) { ["5 5", "1 2\n", "1 0\n", "2 2\n", "2 3\n", "XXNNESEESWNWW"] } 
+    before { allow(File).to receive(:readlines).and_return(invalid_coords) }
+    it     { expect{subject}.to raise_error('Please provide valid instructions - see readme!') }
+  end
+end
